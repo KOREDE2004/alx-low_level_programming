bd59a0f7c1bf5fc6e346fe3fa636ffa2121db4b3
KOREDE2004 <nugaakorede@gmail.com>
2022-09-09 17:12:51 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return Always 0(success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	puts("\"programming is like building a multilingual");
+
+	return (0);
+
+}
